23fa2a3463e3704e697dd617332c7f52c0ea2483
Adil Nadeem <adilnadeem709@gmail.com>
2023-05-17 02:52:53 +0500

    fixed submit button

--- src/app/Components/Contact.tsx
+++ src/app/Components/Contact.tsx
@@ -77,8 +77,8 @@ export default function Contact() {
                     <label htmlFor="message" className='w-2/3 mx-auto text-xl'>Enter your message:</label>
                     <textarea required name="message" className='w-2/3 mx-auto h-40 bg-[#181730] border-2 border-[#EC994B] focus:border-3 focus:outline-none p-2 font-light rounded-lg text-lg text-[#fdfaed]' />
                 </div>
-                <div onMouseEnter={mouseEnter} onMouseLeave={mouseLeave} className={`${activeGradient} flex items-center justify-center w-1/2 md:w-1/4 lg:w-2/12 my-8 mx-auto border-2 rounded-2xl border-[#555297] p-3 text-[#fdfaed] hover:border-[#EC994B] cursor-pointer`}>
-                    <input type='submit' value="Send" className={`rounded-2xl text-center font-semibold text-lg cursor-pointer`} />
+                <div onMouseEnter={mouseEnter} onMouseLeave={mouseLeave} className={`${activeGradient} flex items-center justify-center w-1/2 md:w-1/4 lg:w-2/12 my-8 mx-auto border-2 rounded-2xl border-[#555297] text-[#fdfaed] hover:border-[#EC994B] cursor-pointer`}>
+                    <input type='submit' value="Send" className={`rounded-2xl text-center p-3 font-semibold text-lg cursor-pointer w-full`} />
                 </div>
             </form>
             <ToastContainer
