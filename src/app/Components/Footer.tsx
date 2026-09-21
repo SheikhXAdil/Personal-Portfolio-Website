3bd0d8813255b8c1a1cba3ab3b9a8414509f278f
M. Adil Nadeem <adilnadeemedu@gmail.com>
2026-09-21 23:09:19 +0500

    updated footer

--- src/app/Components/Footer.tsx
+++ src/app/Components/Footer.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Footer() {
     return (
-        <div className='bg-bgSecondary font-extrabold flex items-center justify-center h-20'>
+        <div className='bg-bgSecondary font-bold flex items-center justify-center h-20'>
             © 2023 Muhammad Adil Nadeem. All rights reserved.
         </div>
     )
